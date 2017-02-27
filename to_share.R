6d960b4dad4cf0aa6de8a78a43b2132c6eb65377
Joshua Rosenberg <jrosen@msu.edu>
2017-02-27 18:58:38 -0500

    update post

--- to_share.R
+++ to_share.R
@@ -4,7 +4,8 @@ library(tidyverse)
 library(ggthemes)
 library(gganimate)
 
-df <- read_csv('~/downloads/statedatabyseason56_55.csv') # from https://gis.cdc.gov/grasp/fluview/main.html
+setwd("~/Dropbox/1_Research/cdc-flu-vis")
+df <- read_csv('statedatabyseason56_55.csv') # from https://gis.cdc.gov/grasp/fluview/main.html
 df <- select(df, STATENAME, `ACTIVITY LEVEL`, WEEK, SEASON)
 df <- filter(df, WEEK >= 41 | WEEK <= 7)
 
@@ -30,24 +31,29 @@ df$WEEK <- paste0("Week ", df$WEEK_f, sep = "")
 df$WEEK <- factor(df$WEEK,
                   levels = c(paste0("Week ", c(41:52)), paste0("Week ", c(1:8))))
 
+# df$`Difference Between Years` <- as.factor(df$`Difference Between Years`)
+df$`Difference Between Years`
 gg <- ggplot() +
     geom_map(data = states, map = states,
              aes(x = long, y = lat, map_id = region),
-             color= "white" , size = 0.1) +
+             color= "white", size = 0.1) +
     geom_map(data = df, map = states,
              aes(fill = `Difference Between Years`, map_id = region, frame = WEEK),
              color="white", size = 0.1) +
     coord_map(proj = "bonne", param = 45) +
-    scale_fill_gradient("", high = '#e9a3c9', low = '#a1d76a',
+    # scale_fill_discrete(l = 80, c = 150, guide = guide_legend(reverse = T)) +
+
+    scale_fill_gradient2(high = '#e9a3c9', mid = "gray", low = 'turquoise4',
                         breaks = c(9, 5, 0, -5, -6),
                         labels = c("Higher in 16-17",5, 0, -5,"Higher in 15-16")) +
-    theme_map() +
-    ggtitle("Comparison of Flu Activity Levels") +
-    labs(subtitle = "Data from: https://gis.cdc.gov/grasp/fluview/main.html. Flu activity is measured on a 1-10 scale.",
-         caption = "Created by: http://jrosen48.github.io.") +
-    theme()
-
+    theme_map()
+    # ggtitle("Comparison of Flu Activity Levels") +
+    # labs(subtitle = "Data from: https://gis.cdc.gov/grasp/fluview/main.html. Flu activity is measured on a 1-10 scale.",
+    #      caption = "Created by: http://jrosen48.github.io.")
 gg
 
-animation::ani.options(ani.width=600, ani.height=420)
-gganimate(filename = "final_draft.mp4")
+gg <- gg + theme(legend.position = "none")
+gg
+
+animation::ani.options(ani.width=600, ani.height=400, title = "Difference in Flu Activity Between 15-16 and 16-17 Flu Seasons")
+gganimate(gg, filename = "first_version.gif")
